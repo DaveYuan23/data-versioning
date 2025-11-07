a50f0d68e47fa00252c973aa3b70ae7f390cfe37
DaveYuan23 <daveyuan2002@gmail.com>
2025-11-07 14:24:09 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,10 +4,10 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="cheese-app-data-versioning-v1"
+export GCP_PROJECT="aic215"
 export GCP_ZONE="us-central1-a"
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/secrets/ml-workflow.json"
 
 
 echo "Building image"
