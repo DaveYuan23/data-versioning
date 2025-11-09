b6859905c539d8924a59fe1c149c60d8949a4e19
DaveYuan23 <daveyuan2002@gmail.com>
2025-11-09 10:30:19 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -27,8 +27,8 @@ echo "Running container"
 # -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
 
 docker run --rm --name data-version-cli -ti \
+--cap-add SYS_ADMIN \
 --device /dev/fuse \
--v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
