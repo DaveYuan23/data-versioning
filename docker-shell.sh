153197c6dd7cd95b6e1a5e0c99aed792b8d0da33
DaveYuan23 <daveyuan2002@gmail.com>
2025-11-09 15:16:54 +0000

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -14,18 +14,6 @@ echo "Building image"
 docker build -t data-version-cli -f Dockerfile .
 
 echo "Running container"
-# docker run --rm --name data-version-cli -ti \
-# --privileged \
-# --cap-add SYS_ADMIN \
-# --device /dev/fuse \
-# -v "$BASE_DIR":/app \
-# -v "$SECRETS_DIR":/secrets \
-# #-v ~/.gitconfig:/etc/gitconfig \
-# -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
-# #-e GCP_PROJECT=$GCP_PROJECT \
-# #-e GCP_ZONE=$GCP_ZONE \
-# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
-
 docker run --rm --name data-version-cli -ti \
 --privileged \
 --cap-add SYS_ADMIN \
@@ -34,4 +22,16 @@ docker run --rm --name data-version-cli -ti \
 -v "$SECRETS_DIR":/secrets \
 -v ~/.gitconfig:/etc/gitconfig \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCP_ZONE=$GCP_ZONE \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
+
+# docker run --rm --name data-version-cli -ti \
+# --privileged \
+# --cap-add SYS_ADMIN \
+# --device /dev/fuse \
+# -v "$BASE_DIR":/app \
+# -v "$SECRETS_DIR":/secrets \
+# -v ~/.gitconfig:/etc/gitconfig \
+# -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
